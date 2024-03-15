62d411170845da1e4efb9853d040306e1a1623e3
Aahil13 <onyeanunaprince@example.com>
2024-03-15 13:02:36 +0100

    Refactor fetch request in App.js

--- react/src/App.js
+++ react/src/App.js
@@ -1,21 +1,26 @@
-import { useEffect, useState } from 'react'
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [reviews, setReviews] = useState([])
+  const [reviews, setReviews] = useState([]);
   useEffect(() => {
-    fetch('http://localhost:4000/')
-      .then(res => res.json())
-      .then(data => setReviews(data))
-  }, [])
+    fetch("http://localhost:4000/")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.json();
+      })
+      .then((data) => setReviews(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>all Reviews</h1>
-        {reviews && reviews.map(blog => (
-          <div key={blog.id}>{blog.title}</div>
-        ))}
+        {reviews &&
+          reviews.map((blog) => <div key={blog.id}>{blog.title}</div>)}
       </header>
     </div>
   );
